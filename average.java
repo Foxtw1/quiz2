c751a7cc5817cfc4f9e66560ae8845909631a2fa
Trevor William Fox <foxtw@s.dcsdk12.org>
2018-08-20 07:57:57 -0600

    Can find average of 5 numbers without use of floats

---
+++ average.java
@@ -0,0 +1,17 @@
+class average {
+
+
+    public static void main(String[] args){
+
+
+        int a = 3;
+        int b = 4;
+        int c = 6;
+        int d = 7;
+        int e = 9;
+
+
+        System.out.println((a + b + c + d + e) / 5);
+
+    }
+}
