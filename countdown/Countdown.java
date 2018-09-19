4071883f50f6ef9e9d20e4e899aec474d6682ae6
Trevor Fox <foxtw@s.dcsdk12.org>
2018-09-19 08:35:35 -0600

    can countdown to launch

---
+++ countdown/Countdown.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+class Countdown {
+
+    public static void main(String[] args) {
+        
+        java.util.Scanner scan = new Scanner(System.in);
+        System.out.println("How long until lauch in minutes?");
+        int min = scan.nextInt();
+
+
+        int sec = 0;
+
+        
+        
+        while (min >= 0){
+            while (sec > 0 && sec < 60) {
+
+                System.out.println("T-minus " + min + " minute " + sec + " seconds to launch");
+
+                sec = sec - 10;
+
+            }
+            while ((sec == 0 || sec == 60) && min > 0) {
+
+                
+                System.out.println("T-minus " + min + " minutes to launch");
+                min = min - 1;
+
+                sec = 50;
+
+
+            }
+
+            if (min == 0 && sec == 0) {
+                System.out.println("Missile Away!");
+                break;
+            }
+
+        }
+
+
+
+
+    }
+
+
+}
